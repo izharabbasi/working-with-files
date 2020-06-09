25f797dcd6dc1fdde28cfdf7750b7433a79a7859
Izhar Abaasi <izharabbasi103@gmail.com>
2020-06-09 14:18:24 +0500

    add simple example

--- file.py
+++ file.py
@@ -1,1 +1,5 @@
-print("hello world")
+with open("salary.txt", "r") as rf:
+    with open("salary1.txt", "a") as wf:
+        for line in rf.readlines():
+            name, salary = line.split(",")
+            wf.write(f"{name} and salary is {salary}\n")
