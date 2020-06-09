6a0debc9502c2e73662068af7352c4d603f7f0d7
Izhar Abaasi <izharabbasi103@gmail.com>
2020-06-09 15:21:15 +0500

    add second example

--- writecsv.py
+++ writecsv.py
@@ -2,7 +2,10 @@ from csv import writer
 
 with open("data.csv", "a") as f:
     data = writer(f)
-    data.writerow(["name", "country"])
-    data.writerow(["izhar", "pak"])
-    data.writerow(["tom", "usa"])
-    data.writerow(["meto", "aus"])
+    # data.writerow(["name", "country"])
+    # data.writerow(["izhar", "pak"])
+    # data.writerow(["tom", "usa"])
+    # data.writerow(["meto", "aus"])
+
+    data.writerows(
+        [["name", "country"], ["name", "country"], ["name", "country"]])
